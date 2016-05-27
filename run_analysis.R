dccb8a3bd5631a1e2ba067f1b224a814885bb8ba
SethT <seth@setht.com>
2016-05-26 22:33:08 -0700

    updated to aggregate on all columns

--- run_analysis.R
+++ run_analysis.R
@@ -3,16 +3,16 @@ library(data.table)
 library(tidyr)
 file_prefix <- ""
 
-subject_test <- read.table(paste(file_prefix, "test\\subject_test.txt", sep = ""), header=FALSE)
-X_test <- read.table(paste(file_prefix, "test\\X_test.txt", sep = ""), header=FALSE)
-y_test <- read.table(paste(file_prefix, "test\\y_test.txt", sep = ""), header=FALSE)
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", header=FALSE)
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt", header=FALSE)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", header=FALSE)
 
-subject_train <- read.table(paste(file_prefix, "train\\subject_train.txt", sep = ""), header=FALSE)
-X_train <- read.table(paste(file_prefix, "train\\X_train.txt", sep = ""), header=FALSE)
-y_train <- read.table(paste(file_prefix, "train\\y_train.txt", sep = ""), header=FALSE)
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", header=FALSE)
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt", header=FALSE)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", header=FALSE)
 
-activity_labels <- read.table(paste(file_prefix, "activity_labels.txt", sep = ""), header=FALSE)
-features <- read.table(paste(file_prefix, "features.txt", sep = ""), header=FALSE)
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", header=FALSE)
+features <- read.table("UCI HAR Dataset/features.txt", header=FALSE)
 
 # step 1 merge the test and train datatests
 subject_merged <- rbind(subject_test, subject_train)
@@ -35,16 +35,14 @@ X_data <- inner_join(X_data, y_labels, by = c("id"))
 
 # step 4 label the variable names
 setnames(X_data, old = paste("V", features_mean_or_std$V1, sep = ""), new = as.character(features_mean_or_std$V2))
-X_data.tidy <- gather(X_data, Calculation, Measurement, -id, -Activity)
-X_data.tidy <- select(X_data.tidy, -id)
 
 
 # step 5 avg each variable for each subject and each activity
 subject_merged <- mutate(subject_merged, id = 1:n())
 subject_merged <- rename(subject_merged, Subject = V1)
 X_data_subjects <- inner_join(X_data, subject_merged, by = c("id"))
-X_data_subjects.tidy <- gather(X_data_subjects, Calculation, Measurement, -id, -Activity, -Subject)
-X_data_subjects.tidy <- select(X_data_subjects.tidy, -id)
-X_data_subjects_agg <- summarise(group_by(X_data_subjects.tidy, Activity, Subject), avg = mean(Measurement))
+#X_data_subjects.tidy <- gather(X_data_subjects, Calculation, Measurement, -id, -Activity, -Subject)
+X_data_subjects <- select(X_data_subjects, -id)
+X_data_subjects_agg <- summarise_each(group_by(X_data_subjects, Activity, Subject), funs(mean))
 
 write.table(X_data_subjects_agg, "X_data_subjects_agg.txt", row.name = FALSE)
